a2e07c9319aae4973f615ae01a7d9083bae40b88
Rahul Srivastava <rahul4ror@gmail.com>
2019-01-02 23:42:10 +0530

    Added user new edit and update function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_125114) do
+ActiveRecord::Schema.define(version: 2019_01_02_110542) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_12_25_125114) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
